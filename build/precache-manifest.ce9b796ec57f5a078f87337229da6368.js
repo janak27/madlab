e0e19c52c317250c9e02ec5bce2792e6619a4406
JANAK BABAN AVHAD <80969920+janak27@users.noreply.github.com>
2022-04-04 03:44:24 +0530

    final version

---
+++ build/precache-manifest.ce9b796ec57f5a078f87337229da6368.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41528dab3180b720e698f6534563d900",
+    "url": "/madlab/index.html"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/css/2.9be35272.chunk.css"
+  },
+  {
+    "revision": "f5335e71e03eb251bddb",
+    "url": "/madlab/static/css/main.e0e55565.chunk.css"
+  },
+  {
+    "revision": "467f85b604a3f1524c26",
+    "url": "/madlab/static/js/2.903e3fb3.chunk.js"
+  },
+  {
+    "revision": "f5335e71e03eb251bddb",
+    "url": "/madlab/static/js/main.50a9668b.chunk.js"
+  },
+  {
+    "revision": "396f417b8273cab40f0e",
+    "url": "/madlab/static/js/runtime~main.05b0808b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/madlab/static/media/logo.9f7e506d.svg"
+  }
+]);
